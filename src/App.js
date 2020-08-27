95acd5955510e0426518e315766ce5f2237f9788
Xavier Ritch <xritch89@hotmail.com>
2020-08-27 14:06:46 -0400

    list component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import List from './List'
+import STORE from './STORE'
 import './App.css';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
+    state = { 
+    store: STORE
+   }
+
+   handleDeleteCard(id){
+     console.log('handleDeleteCard triggered')
+      STORE.filter((item) => item.id !== id);
+   }
 
   render() {
-    const { store } = this.props
+    const { store } = this.state
     return (
       <main className='App'>
         <header className='App-header'>
@@ -20,6 +23,7 @@ class App extends Component {
         <div className='App-list'>
           {store.lists.map(list => (
             <List
+              onDeleteCard={this.handleDeleteCard}
               key={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Store from './STORE'
 import './Card.css';
 
-export default function Card(props) {
-  return (
-    <div className='Card'>
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      deleteClicked: false
+     }
+
+     const handleDeleteCard = (id) => {
+      Store.filter((item) => item.id !== id);
+      this.setState({
+        deleteClicked: true
+      })
+     }
+  }
+
+  render() { 
+    return ( 
+      <div className='Card'>
       <button
         type='button'
+        onClick={this.props.onDeleteCard}
       >
         delete
       </button>
-      <h3>{props.title}</h3>
-      <p>{props.content}</p>
+      <h3>{this.props.title}</h3>
+      <p>{this.props.content}</p>
     </div>
-  )
+     );
+  }
 }
+ 
+export default Card;
